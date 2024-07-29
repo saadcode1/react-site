5f83107c64b9120474e02a88eeef6d603dc30278
saadcode1 <163667486+saadcode1@users.noreply.github.com>
2024-07-29 15:27:04 +0530

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,42 +1,47 @@
-const mongoose = require("mongoose");
-const User=require("../models/user");
-const Post =require("../models/post");
-module.exports.signin=async (req,res)=>{
-    try{ 
-        let {email,username,password}=req.body;
-          let ragisterUser=new User({
-                email,
-                username,
-          })
-         let user= await User.register(ragisterUser,password);
-         req.login(user,(err)=>{
-                if(err){
-                  console.log(err);
-                  return next(err);
-                }
-            });
-          
-            res.status(201).send(req.user);
-        }catch(err){
-              res.status(401).send(err.message);
-        }
+const User = require("../models/user");
+const Post = require("../models/post");
+
+module.exports.signin = async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const newUser = new User({ email, username });
+    const user = await User.register(newUser, password);
+    req.login(user, (err) => {
+      if (err) {
+        return res.status(401).send(err.message);
+      }
+      res.status(201).send(req.user);
+    });
+  } catch (err) {
+    res.status(401).send(err.message);
+  }
 }
-module.exports.loggedIn=(req,res)=>{
-        try{ user=req.user;
-          res.status(201).send(req.user);
-        }catch(e){
-          res.status(401).send(e.message);
-        }
+
+module.exports.loggedIn = (req, res) => {
+  try { console.log(req.user);
+    res.status(201).send(req.user);
+  } catch (e) {
+    res.status(401).send(e.message);
+  }
 }
 
 module.exports.postCreated = async (req, res) => {
-  try { 
+  try {
     const { description, image } = req.body;
-    const post = new Post({ description, post: image, userId:req.user});
+    const post = new Post({ description, post: image, userId: req.user._id });
     await post.save();
     res.status(201).send(post);
   } catch (e) {
-    console.log(e.message);
-    res.status(500).send({ message: 'Failed to create post' });
+    console.error(e);
+    res.status(500).send({ message: 'Internal Server Error' });
   }
 };
+
+
+module.exports.loggeOut=(req,res)=>{
+  req.logout(function(err) {
+    if (err) { res.status(401).send(err); }
+    console.log("you are successfully loggedOut!");
+   return res.status(201).send("successfully loggedOut");
+  });
+}
